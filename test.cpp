64e50942d24737a97157cfbd8f280912995967a4
FeherJano <feherjanos01@gmail.com>
2024-09-09 20:43:57 +0200

    player movement added

--- test.cpp
+++ test.cpp
@@ -13,11 +13,51 @@ const int HEIGHT = 21; // A labirintus magassága
 int dx[] = {-2, 2, 0, 0};
 int dy[] = {0, 0, -2, 2};
 
+// Játékos osztály
+class Player {
+public:
+    int x, y; // Játékos pozíciója
+
+    Player(int startX, int startY) : x(startX), y(startY) {}
+
+    // Játékos mozgatása, ha az új pozíció érvényes
+    void move(char direction, const vector<vector<int>>& maze) {
+        int newX = x;
+        int newY = y;
+
+        if (direction == 'w') newX--;
+        if (direction == 's') newX++;
+        if (direction == 'a') newY--;
+        if (direction == 'd') newY++;
+
+        // Csak akkor mozgatjuk, ha az új pozíció nem fal (azaz 1-es)
+        if (newX >= 0 && newX < HEIGHT && newY >= 0 && newY < WIDTH && maze[newX][newY] == 1) {
+            x = newX;
+            y = newY;
+        }
+    }
+
+    // Játékos pozíciójának megjelenítése
+    void displayPosition() const {
+        cout << "Játékos pozíció: (" << x << ", " << y << ")" << endl;
+    }
+};
+
 // A labirintus megjelenítése a konzolon
-void printMaze(const vector<vector<int>>& maze) {
+void printMaze(const vector<vector<int>>& maze, const vector<Player>& players) {
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
-            cout << (maze[i][j] == 0 ? "#" : " ");
+            bool isPlayerHere = false;
+            for (const Player& player : players) {
+                if (player.x == i && player.y == j) {
+                    cout << "P"; // Játékos pozíciója
+                    isPlayerHere = true;
+                    break;
+                }
+            }
+            if (!isPlayerHere) {
+                cout << (maze[i][j] == 0 ? "#" : " ");
+            }
         }
         cout << endl;
     }
@@ -58,8 +98,27 @@ int main() {
     // A labirintus generálása
     generateMaze(maze, 1, 1);
 
-    // A labirintus kiírása
-    printMaze(maze);
+    // Játékos hozzáadása
+    vector<Player> players;
+    players.push_back(Player(1, 1)); // Első játékos kezdőpozíciója
+
+    char input;
+    while (true) {
+        // Labirintus megjelenítése
+        printMaze(maze, players);
+
+        // Játékos mozgatása
+        cout << "Mozgás (w/a/s/d, q a kilépéshez): ";
+        cin >> input;
+
+        if (input == 'q') break; // Kilépés
+
+        // Játékos 0 mozgatása
+        players[0].move(input, maze);
+
+        // Játékos aktuális pozíciója
+        players[0].displayPosition();
+    }
 
     return 0;
 }
